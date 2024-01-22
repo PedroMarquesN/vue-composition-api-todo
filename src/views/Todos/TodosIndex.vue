d44d2fbac8514bd229f0e783301316aa8c3262ba
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-01-22 15:47:36 -0300

    Aula composiçao api  02

--- src/views/Todos/TodosIndex.vue
+++ src/views/Todos/TodosIndex.vue
@@ -1,20 +1,26 @@
 <script>
 import TodoService from '@/services/todo.services'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 
 
 export default {
  name: 'TodosIndex',
  setup(){
 
+     const todos = ref([])
 
     onMounted(() => {
+
         TodoService.getAll()
                 .then(response =>{
                     console.log(response)
+                    todos.value = response.data.data
                 })
                 .catch(error => console.log(error))
     })
+    return {
+        todos,
+    }
 
  }
 }
@@ -22,4 +28,10 @@ export default {
 
 <template>
     <h1>Lista de tarefas</h1>
+
+    <ul>
+        <li v-for="todo in todos" :key="todo.indentify">
+            {{ todo.title }}
+        </li>
+    </ul>
 </template>
